48c6c43e3c3db25c16e3e51a1a60c58ce3a5aac4
Annie Nguyen <30813052+mnghinguyen@users.noreply.github.com>
2022-08-17 21:42:48 -0400

    modify css in js

--- assets/seatgeek.js
+++ assets/seatgeek.js
@@ -93,7 +93,7 @@ function getEventInfo(ID) {
                 <tr class="bg-indigo-100 border-b">
                     <th scope="row" class="py-4 px-6 font-bold text-indigo-900 whitespace-nowrap">
                         <span class="text-sm text-indigo-900" id="event-title">${eventTitle}</span></br>
-                        <span class="text-sm text-indigo-900 font medium" id="artist">${artist}</span>
+                        <span class="text-sm text-indigo-900 font-medium" id="artist">${artist}</span>
                     </th>
                     <td class="py-4 px-6 text-indigo-900">
                          <span class="text-m text-indigo-900" id="venue-name">${venueName}</span> <br>
